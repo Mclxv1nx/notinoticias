a2febd5f40b299356432f59289c0723be4d4213a
Adrian Urresta <laurrestas@utn.edu.ec>
2026-07-12 09:47:35 -0500

    Eliminar .bat

--- iniciar-notinoticias.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-REM Doble clic para abrir el reportaje en http://notinoticias (solo en esta laptop)
-powershell -NoProfile -ExecutionPolicy Bypass -File "%~dp0notinoticias.ps1"
-pause
--- quitar-notinoticias.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-REM Elimina "notinoticias" del archivo hosts (revierte el cambio)
-powershell -NoProfile -ExecutionPolicy Bypass -File "%~dp0quitar-notinoticias.ps1"
-pause
